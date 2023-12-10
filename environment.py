706c4736918e0d1c3f8d7dc2a0026f8bbd806c3a
Seldre99 <a.selice99@gmail.com>
2023-12-10 11:25:06 +0100

    movimenti e turni 1.0

--- environment.py
+++ environment.py
@@ -18,79 +18,79 @@ def disegna_tabella(finestra, dimensione_cella, spessore_bordo):
     for riga in range(15):
         for colonna in range(15):
             #Caselle Rosse
-            if (riga == 0 and colonna in range(6)):
+            if riga == 0 and colonna in range(6):
                 colore_casella = (255, 0, 0)
-            elif (riga in range(6) and colonna == 0):
+            elif riga in range(6) and colonna == 0:
                 colore_casella = (255, 0, 0)
-            elif (riga == 5 and colonna in range(6)):
+            elif riga == 5 and colonna in range(6):
                 colore_casella = (255, 0, 0)
-            elif (riga in range(6) and colonna == 5):
+            elif riga in range(6) and colonna == 5:
                 colore_casella = (255, 0, 0)
-            elif ((riga == 6 or riga == 7) and colonna == 1):
+            elif (riga == 6 or riga == 7) and colonna == 1:
                 colore_casella = (255, 0, 0)
-            elif (riga == 7 and colonna in range(2,7)):
+            elif riga == 7 and colonna in range(2,7):
                 colore_casella = (255, 0, 0)
 
             #Caselle per i pallini rossi
-            elif ((riga == 2 or riga == 3) and (colonna == 2 or colonna == 3)):
+            elif (riga == 2 or riga == 3) and (colonna == 2 or colonna == 3):
                 colore_casella = (255, 0, 0)
 
             #Caselle verdi
-            elif (riga == 0 and colonna in range(9,15)):
+            elif riga == 0 and colonna in range(9,15):
                 colore_casella = (0, 255, 0)
-            elif (riga in range(6) and colonna == 9):
+            elif riga in range(6) and colonna == 9:
                 colore_casella = (0, 255, 0)
-            elif (riga == 5 and colonna in range(9,15)):
+            elif riga == 5 and colonna in range(9,15):
                 colore_casella = (0, 255, 0)
-            elif (riga in range(6) and colonna == 14):
+            elif riga in range(6) and colonna == 14:
                 colore_casella = (0, 255, 0)
-            elif (riga == 1 and (colonna == 7 or colonna == 8)):
+            elif riga == 1 and (colonna == 7 or colonna == 8):
                 colore_casella = (0, 255, 0)
-            elif (riga in range(1,7) and colonna == 7):
+            elif riga in range(1,7) and colonna == 7:
                 colore_casella = (0, 255, 0)
 
             # Caselle per i pallini verdi
-            elif ((riga == 2 or riga == 3) and (colonna == 11 or colonna == 12)):
+            elif (riga == 2 or riga == 3) and (colonna == 11 or colonna == 12):
                 colore_casella = (0, 255, 0)
 
             #Caselle blu
-            elif (riga == 9 and colonna in range(6)):
+            elif riga == 9 and colonna in range(6):
                 colore_casella = (0, 0, 255)
-            elif (riga in range(9,15) and colonna == 0):
+            elif riga in range(9,15) and colonna == 0:
                 colore_casella = (0, 0, 255)
-            elif (riga == 14 and colonna in range(6)):
+            elif riga == 14 and colonna in range(6):
                 colore_casella = (0, 0, 255)
-            elif (riga in range(9,15) and colonna == 5):
+            elif riga in range(9,15) and colonna == 5:
                 colore_casella = (0, 0, 255)
-            elif (riga == 13 and (colonna == 6 or colonna == 7)):
+            elif riga == 13 and (colonna == 6 or colonna == 7):
                 colore_casella = (0, 0, 255)
-            elif (riga in range(8,14) and colonna == 7):
+            elif riga in range(8,14) and colonna == 7:
                 colore_casella = (0, 0, 255)
 
             #Caselle per i pallini blu
-            elif ((riga == 11 or riga == 12) and (colonna == 2 or colonna == 3)):
+            elif (riga == 11 or riga == 12) and (colonna == 2 or colonna == 3):
                 colore_casella = (0, 0, 255)
 
             #Caselle gialle
-            elif (riga == 9 and colonna in range(9,15)):
+            elif riga == 9 and colonna in range(9,15):
                 colore_casella = (255, 255, 0)
-            elif (riga in range(9,15) and colonna == 9):
+            elif riga in range(9,15) and colonna == 9:
                 colore_casella = (255, 255, 0)
-            elif (riga == 14 and colonna in range(9,15)):
+            elif riga == 14 and colonna in range(9,15):
                 colore_casella = (255, 255, 0)
-            elif (riga in range(9,15) and colonna == 14):
+            elif riga in range(9,15) and colonna == 14:
                 colore_casella = (255, 255, 0)
-            elif ((riga == 7 or riga == 8) and colonna == 13):
+            elif (riga == 7 or riga == 8) and colonna == 13:
                 colore_casella = (255, 255, 0)
-            elif (riga == 7 and colonna in range(8,14)):
+            elif riga == 7 and colonna in range(8,14):
                 colore_casella = (255, 255, 0)
 
             #Caselle per i pallini blu
-            elif ((riga == 11 or riga == 12) and (colonna == 11 or colonna == 12)):
+            elif (riga == 11 or riga == 12) and (colonna == 11 or colonna == 12):
                 colore_casella = (255, 255, 0)
 
             #Caselle nere da cui non passare
-            elif ((riga == 6 or riga == 8) and (colonna == 6 or colonna == 8)):
+            elif (riga == 6 or riga == 8) and (colonna == 6 or colonna == 8):
                 colore_casella = (0, 0, 0)
 
             else:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from pedina import Token
 from environment import disegna_tabella, Dice
-import random
+import time
 
 # Inizializza Pygame
 pygame.init()
@@ -13,30 +13,89 @@ altezza_finestra = 600
 colore_sfondo = (255, 255, 255)
 dimensione_cella = larghezza_finestra // 15  # 15 caselle in una riga
 spessore_bordo = 1
+phase = "red"
+turno_player_red = True
+tempo_limite = 30
+tempo_iniziale = time.time()
 
 ####### TOKENS ########
 # Create four tokens
 tokens = [Token((139,0,0), (2, 2), 1, dimensione_cella),
           Token((139,0,0), (2, 3), 2, dimensione_cella),
-          Token((173, 216, 230), (11, 2), 1, dimensione_cella),
-          Token((173, 216, 230), (11, 3), 2, dimensione_cella),
           Token((0, 100, 0), (2, 11), 1, dimensione_cella),
-          Token((0, 100, 0), (2, 12), 2, dimensione_cella),
-          Token((184, 134, 11), (11, 11), 1, dimensione_cella),
-          Token((184, 134, 11), (11, 12), 2, dimensione_cella)]
+          Token((0, 100, 0), (2, 12), 2, dimensione_cella)]
 
 dado = Dice((0, 0, 0), (7, 7), 30)
 # Inizializza la finestra
 finestra = pygame.display.set_mode((larghezza_finestra, altezza_finestra))
 pygame.display.set_caption("Tabella Ludo")
 
+# Scelta del token da spostare
+def turn(phase, tok):
+    if phase == "red" and tok == 1:
+        if tokens[0].position == (2, 2) and dado.value == 6:
+            tokens[0].position = (6, 2)
+        elif tokens[0].position != (2, 2):
+            tokens[0].move(dado.value)
+    elif phase == "red" and tok == 2:
+        if tokens[1].position == (2, 3) and dado.value == 6:
+            tokens[1].position = (6, 2)
+        elif tokens[1].position != (2, 3):
+            tokens[1].move(dado.value)
+    elif phase == "green" and tok == 1:
+        if tokens[2].position == (2, 11) and dado.value == 6:
+            tokens[2].position = (2, 8)
+        elif tokens[2].position != (2, 11):
+            tokens[2].move(dado.value)
+    elif phase == "green" and tok == 2:
+        if tokens[3].position == (2, 12) and dado.value == 6:
+            tokens[3].position = (2, 8)
+        elif tokens[3].position != (2, 12):
+            tokens[3].move(dado.value)
+    return dado
+
 # Ciclo principale
 while True:
+
+    tempo_trascorso = time.time() - tempo_iniziale
+    if tempo_trascorso > tempo_limite:
+        print("Tempo scaduto! Riprova.")
+        dado.roll()
+        tempo_iniziale = time.time()
+
+    # Pulisci la finestra
+    finestra.fill(colore_sfondo)
+
+    # Disegna la tabella
+    disegna_tabella(finestra, dimensione_cella, spessore_bordo)
+
+    # Draw the dice
+    pygame.draw.rect(finestra, dado.color, (
+    dado.position[1] * dimensione_cella, dado.position[0] * dimensione_cella, dimensione_cella, dimensione_cella), 0)
+    pygame.draw.rect(finestra, (0, 0, 0), (
+    dado.position[1] * dimensione_cella, dado.position[0] * dimensione_cella, dimensione_cella, dimensione_cella),
+                     spessore_bordo)
+    if phase == "green":
+        number = pygame.font.SysFont(None, 30).render(str(dado.value), True, (255, 0, 0))
+    else:
+        number = pygame.font.SysFont(None, 30).render(str(dado.value), True, (0, 255, 0))
+    number_rect = number.get_rect(center=(dado.position[1] * dimensione_cella + dimensione_cella // 2,
+                                          dado.position[0] * dimensione_cella + dimensione_cella // 2))
+    finestra.blit(number, number_rect)
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_w or evento.key == pygame.K_e:
+                tok = 1 if evento.key == pygame.K_w else 2
+                phase = "red" if turno_player_red else "green"
+                dado = turn(phase, tok)
+                turno_player_red = not turno_player_red
+                dado.roll()
+                tempo_iniziale = time.time()
+            '''
             # Check arrow keys for movement
             if evento.key == pygame.K_UP:
                 current_pos, previous_pos = tokens[0].move('UP')
@@ -54,13 +113,7 @@ while True:
                 if evento.key == pygame.K_SPACE:
                     dado.roll()
                     print(f'Dice Rolled. Value: {dado.value}')
-
-
-    # Pulisci la finestra
-    finestra.fill(colore_sfondo)
-
-    # Disegna la tabella
-    disegna_tabella(finestra, dimensione_cella, spessore_bordo)
+            '''
 
     # Disegna i tokens
     for token in tokens:
@@ -68,19 +121,9 @@ while True:
         center_x = y * dimensione_cella + dimensione_cella // 2
         center_y = x * dimensione_cella + dimensione_cella // 2
         pygame.draw.circle(finestra, token.color, (center_x, center_y), token.size)
-        number = pygame.font.SysFont(None, 30).render(str(token.number), True, (0,0,0))
+        number = pygame.font.SysFont(None, 30).render(str(token.number), True, (0, 0, 0))
         number_rect = number.get_rect(center=(center_x, center_y))
         finestra.blit(number, number_rect)
 
-    # Draw the dice
-    pygame.draw.rect(finestra, dado.color, (
-    dado.position[1] * dimensione_cella, dado.position[0] * dimensione_cella, dimensione_cella, dimensione_cella), 0)
-    pygame.draw.rect(finestra, (0, 0, 0), (
-    dado.position[1] * dimensione_cella, dado.position[0] * dimensione_cella, dimensione_cella, dimensione_cella),
-                     spessore_bordo)
-    number = pygame.font.SysFont(None, 30).render(str(dado.value), True, (255, 255, 255))
-    number_rect = number.get_rect(center=(dado.position[1] * dimensione_cella + dimensione_cella // 2,
-                                          dado.position[0] * dimensione_cella + dimensione_cella // 2))
-    finestra.blit(number, number_rect)
     # Aggiorna la finestra
     pygame.display.flip()
